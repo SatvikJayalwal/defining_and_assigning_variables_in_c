44d32bb8b62069322fc04479985d40c5542d6f6e
Satvik Jayalwal <69150647+SatvikJayalwal@users.noreply.github.com>
2022-03-12 15:51:00 +0530

    Add files via upload

---
+++ pic-prac2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a=5;
+   float b=9.5;
+   char c[15]="Polytechnic";
+   printf("\nThis program represents defining and assigning variables\n");
+   printf("\nInteger : %d",a);
+   printf("\nFloat : %f",b);
+   printf("\nCharacter : %s",c);
+   return 0;
+}
